6c56283a1473a9a9a61684516aec468c1929a6da
Cas <cas@interglobal.vision>
2018-09-28 18:59:53 -0500

    fix fetchYoutube test

--- functions/fetchYoutube.test.js
+++ functions/fetchYoutube.test.js
@@ -5,3 +5,5 @@ const data = {
 }
 
 fetchSubscriptionInfo(data);
+
+// fetchSubscriptionInfo({ "parsed" : false, "regex" : "MV|M\\V|M/V", "url" : "https://www.youtube.com/channel/UCDIhSUQ7tY5yfSKJWeNaPUQ?view_as=subscriber" });
