0ed02dfda42de4a9466f33c5387f8c26b010f26f
Cas <cas@interglobal.vision>
2018-10-29 12:17:30 -0600

    lint migration

--- migrations/migration.js
+++ migrations/migration.js
@@ -1,60 +1,58 @@
 const fs = require('fs');
 const _ = require('underscore')
 
-const db = require('./sample.json')
+const db = require('./input.json')
 
 const migrate = (db, version) => {
 
   const newDB = MIGRATIONS[version](db)
 
   console.log(newDB)
-  debugger
-  fs.writeFile("output.json", JSON.stringify(newDB), 'utf8', function (err) {
-    debugger
+  fs.writeFile('migrated.json', JSON.stringify(newDB), 'utf8', function (err) {
     if (err) {
-      console.log("An error occured while writing JSON Object to File.");
+      console.log('An error occured while writing JSON Object to File.');
       return console.log(err);
     }
 
-    console.log("JSON file has been saved.");
+    console.log('JSON file has been saved.');
   });
 }
 
 const MIGRATIONS = {
- 1: db => {
-   const tracks = db.tracks;
+  1: db => {
+    const tracks = db.tracks;
 
-   let transform = {}
+    let transform = {}
 
-   Object.keys(tracks).forEach( key => {
-     const track = tracks[key];
+    Object.keys(tracks).forEach( key => {
+      const track = tracks[key];
 
-     let status = 'unpublished'
+      let status = 'unpublished'
 
-     const filter = /MV|M\V|M\/V|Music Video/g
+      const filter = /MV|M\V|M\/V|Music Video/g
 
-     if(filter.test(track.title)) {
-       status = 'published'
-     }
+      if(filter.test(track.title)) {
+        status = 'published'
+      }
 
 
-     transform[key] = Object.assign(
-       {},
-       _.omit(track, 'draft', 'public'),
-       {
-         status,
-       }
-     )
-   })
+      transform[key] = Object.assign(
+        {},
+        _.omit(track, 'draft', 'public'),
+        {
+          status,
+        }
+      )
+    })
 
-   return Object.assign(
-     {},
-     db,
-     {
-       'tracks': transform,
-     },
-   )
- }
+    return Object.assign(
+      {},
+      db,
+      {
+        'tracks': transform,
+      },
+    )
+  },
 }
 
 migrate(db, 1);
